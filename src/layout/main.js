71605b34a84f6f2bd691144e04fd9a6c0ae18cc2
waraaaho <howardho1d@gmail.com>
2024-07-10 19:10:01 +0100

    log the current location

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from '../pages/header/header'
 import Footer from '../pages/footer/footer'
 import './main.scss'
@@ -7,6 +7,8 @@ import './main.scss'
 import mainRoutes from '../routes/main'
 
 function MainLayout() {
+    const location = useLocation(); // Use the useLocation hook here
+    console.log("location",location.pathname);    
     return (
         <div className="main">
             <Header />
